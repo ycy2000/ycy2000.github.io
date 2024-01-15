b584f462900da50f2bdd3f909be9d5f428d73a7d
ycy2000 <ycy2000@naver.com>
2024-01-15 21:30:59 +0900

    ㄴㅇㄹㄴㅇ

--- portal/assets/js/black_해역사진.js
+++ portal/assets/js/black_해역사진.js
@@ -8,7 +8,7 @@ function 교섭력계산() {
   document.querySelector('#계산_하코저가').value=(일회교섭력*(2.06011)).toFixed(0);
   document.querySelector('#계산_더코').value=(일회교섭력*(2.5494)).toFixed(0);
   document.querySelector('#계산_하코').value=(일회교섭력*(3.0646)).toFixed(0);
-  document.querySelector('#계산_대양저가').value=(일회교섭력*(3.2581)).toFixed(0);
+  document.querySelector('#계산_대양저가').value=(일회교섭력*(3.2582)).toFixed(0);
   document.querySelector('#계산_대양고가').value=(일회교섭력*(4.0727)).toFixed(0);
 
   document.querySelector('#계산_하코4종').value=Number(document.querySelector('#계산_하코저가').value*2)
