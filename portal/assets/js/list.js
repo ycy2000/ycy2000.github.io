90f63d7e7d595740c38a3585964665191c2868b2
ycy2000 <ycy2000@naver.com>
2024-06-03 09:44:40 +0900

    FGDFG

---
+++ portal/assets/js/list.js
@@ -0,0 +1,22 @@
+var 리스너_head_button_group=document.querySelector('#head_button_group');
+function 리스너_head_button_group클릭이벤트(e) {
+  for (var i=0; i<document.querySelectorAll('.카테고리').length; i++) {
+    document.querySelectorAll('.카테고리')[i].classList.remove('active');
+  }
+  if(e.target.classList.contains('카테고리')) {e.target.classList.add('active')}
+
+  document.querySelector('#결과카피').innerHTML='';
+  document.querySelector('#결과카피').classList.add('d-none');
+  if (document.querySelector('#' + e.target.title)) {
+    document.querySelector('#결과카피').innerHTML=document.querySelector('#' + e.target.title).outerHTML;
+    document.querySelector('#결과카피').classList.remove('d-none');
+  }
+
+
+
+}
+
+
+리스너_head_button_group.addEventListener('click', 리스너_head_button_group클릭이벤트);
+
+
