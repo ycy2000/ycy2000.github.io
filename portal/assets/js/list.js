f5955dcafffb2307f238736e5a3d319120e54619
ycy2000 <ycy2000@naver.com>
2024-06-03 17:07:25 +0900

    dfd

--- portal/assets/js/list.js
+++ portal/assets/js/list.js
@@ -1,11 +1,54 @@
+//가로 한줄씩 : var 줄바꿈정보=문자열.split('\n');, 한줄안의 셀들 :줄바꿈한줄정보=줄바꿈정보[i].split('\t');
+function 풀기_리스트() {
+  console.log('풀기_리스트()')
+  document.querySelector('#풀기셑팅').classList.remove('d-none');
+
+    var 문자열=document.querySelector('#풀기_리스트_왼쪽').innerHTML;
+    var 줄바꿈정보=문자열.split('\n'); //\n=줄바꿈 (가로10칸이 하나의 정보)
+    var 줄바꿈한줄정보;
+    var 한줄결과문자열='';
+    var 왼쪽결과합치기문자열='';
+    if (문자열=='') {return;}
+    for (var i=0; i<줄바꿈정보.length-1; i++) {
+      한줄결과문자열='';
+      줄바꿈한줄정보=줄바꿈정보[i].split('\t'); //\t=셀당1개정보
+      for (var 내부=0; 내부<줄바꿈한줄정보.length; 내부++) {
+        한줄결과문자열+='<div>' + 줄바꿈한줄정보[내부] + '</div>';
+      }
+      한줄결과문자열='<div>' + 한줄결과문자열 + '</div>';
+      왼쪽결과합치기문자열=왼쪽결과합치기문자열 + 한줄결과문자열;
+    }
+    왼쪽결과합치기문자열='<div id="js_리스트_왼쪽">' + 왼쪽결과합치기문자열 + '</div>'; //inline-block 설정을 위한 div 감싸기
+
+    var 문자열=document.querySelector('#풀기_리스트_정리').innerHTML;
+    var 줄바꿈정보=문자열.split('\n'); //정리
+    var 줄바꿈한줄정보;
+    var 한줄결과문자열='';
+    var 정리결과합치기문자열='';
+    if (문자열=='') {return;}
+    for (var i=0; i<줄바꿈정보.length-1; i++) {
+      한줄결과문자열='';
+      줄바꿈한줄정보=줄바꿈정보[i].split('\t'); //\t=셀당1개정보
+      for (var 내부=0; 내부<줄바꿈한줄정보.length; 내부++) {
+        한줄결과문자열+='<div>' + 줄바꿈한줄정보[내부] + '</div>';
+      }
+      한줄결과문자열='<div>' + 한줄결과문자열 + '</div>';
+      정리결과합치기문자열=정리결과합치기문자열 + 한줄결과문자열;
+    }
+    정리결과합치기문자열='<div id="js_리스트_정리">' + 정리결과합치기문자열 + '</div>'; //inline-block 설정을 위한 div 감싸기
+
+    document.querySelector('#풀기셑팅').innerHTML=왼쪽결과합치기문자열 + 정리결과합치기문자열;
+}
+
+
+//시작시 작동코드
+풀기_리스트()
 for (var i=0; i<document.querySelectorAll('.카테고리').length; i++) {
   document.querySelectorAll('.카테고리')[i].classList.remove('active');
   if(document.querySelectorAll('.카테고리')[i].title=='결과_리스트') {
     document.querySelectorAll('.카테고리')[i].classList.add('active')
   }
 }
-document.querySelector('#결과카피').innerHTML=document.querySelector('#결과_리스트').outerHTML;
-document.querySelector('#결과카피').classList.remove('d-none');
 
 
 var 리스너_head_button_group=document.querySelector('#head_button_group');
@@ -15,17 +58,10 @@ function 리스너_head_button_group클릭이벤트(e) {
   }
   if(e.target.classList.contains('카테고리')) {e.target.classList.add('active')}
 
-  document.querySelector('#결과카피').innerHTML='';
-  document.querySelector('#결과카피').classList.add('d-none');
-  if (document.querySelector('#' + e.target.title)) {
-    document.querySelector('#결과카피').innerHTML=document.querySelector('#' + e.target.title).outerHTML;
-    document.querySelector('#결과카피').classList.remove('d-none');
+  if (e.target.innerHTML=='리스트') {
+    풀기_리스트();
   }
-
-
-
 }
-
 
 리스너_head_button_group.addEventListener('click', 리스너_head_button_group클릭이벤트);
 
