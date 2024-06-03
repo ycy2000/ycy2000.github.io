26fa51169ca5cc590cbdc6ed997560cbe2d0ac2c
ycy2000 <ycy2000@naver.com>
2024-06-03 10:10:42 +0900

    ㅇㄹㄴㄹ

--- portal/assets/js/list.js
+++ portal/assets/js/list.js
@@ -1,3 +1,13 @@
+for (var i=0; i<document.querySelectorAll('.카테고리').length; i++) {
+  document.querySelectorAll('.카테고리')[i].classList.remove('active');
+  if(document.querySelectorAll('.카테고리')[i].title=='결과_리스트') {
+    document.querySelectorAll('.카테고리')[i].classList.add('active')
+  }
+}
+document.querySelector('#결과카피').innerHTML=document.querySelector('#결과_리스트').outerHTML;
+document.querySelector('#결과카피').classList.remove('d-none');
+
+
 var 리스너_head_button_group=document.querySelector('#head_button_group');
 function 리스너_head_button_group클릭이벤트(e) {
   for (var i=0; i<document.querySelectorAll('.카테고리').length; i++) {
