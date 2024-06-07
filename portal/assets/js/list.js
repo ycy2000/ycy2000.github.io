518699d3e2ab686407b62a12b097c3f2d0a7b44f
ycy2000 <ycy2000@naver.com>
2024-06-07 16:11:43 +0900

    ㅇㄹㄴ

--- portal/assets/js/list.js
+++ portal/assets/js/list.js
@@ -42,51 +42,6 @@ function 풀기_리스트() {
 
     document.querySelector('#풀기셑팅').innerHTML=왼쪽결과합치기문자열 + 정리결과합치기문자열;
 }
-function 풀기_입항관리() {
-  console.log('풀기_입항관리()')
-  document.querySelector('#풀기셑팅').classList.remove('d-none');
-  document.querySelector('#PNG셑팅').classList.add('d-none');
-
-    var 문자열=document.querySelector('#풀기_입항관리').innerHTML;
-    var 줄바꿈정보=문자열.split('\n'); //\n=줄바꿈 (가로10칸이 하나의 정보)
-    var 줄바꿈한줄정보;
-    var 한줄결과문자열='';
-    var 왼쪽결과합치기문자열='';
-    if (문자열=='') {return;}
-    for (var i=0; i<줄바꿈정보.length-1; i++) {
-      한줄결과문자열='';
-      줄바꿈한줄정보=줄바꿈정보[i].split('\t'); //\t=셀당1개정보
-      for (var 내부=0; 내부<줄바꿈한줄정보.length; 내부++) {
-        // if (내부==0 && i==0) {한줄결과문자열+='<div></div>';}
-        // if (내부==0 && i>0) {한줄결과문자열+='<div>' + i + '</div>';}
-        한줄결과문자열+='<div>' + 줄바꿈한줄정보[내부] + '</div>';
-      }
-      한줄결과문자열='<div>' + 한줄결과문자열 + '</div>';
-      왼쪽결과합치기문자열=왼쪽결과합치기문자열 + 한줄결과문자열;
-    }
-    왼쪽결과합치기문자열='<div id="js_입항관리">' + 왼쪽결과합치기문자열 + '</div>'; //inline-block 설정을 위한 div 감싸기
-
-    var 문자열=document.querySelector('#풀기_입항관리_정리').innerHTML;
-    var 줄바꿈정보=문자열.split('\n'); //정리
-    var 줄바꿈한줄정보;
-    var 한줄결과문자열='';
-    var 정리결과합치기문자열='';
-    if (문자열=='') {return;}
-    for (var i=0; i<줄바꿈정보.length-1; i++) {
-      한줄결과문자열='';
-      줄바꿈한줄정보=줄바꿈정보[i].split('\t'); //\t=셀당1개정보
-      for (var 내부=0; 내부<줄바꿈한줄정보.length; 내부++) {
-        한줄결과문자열+='<div>' + 줄바꿈한줄정보[내부] + '</div>';
-      }
-      한줄결과문자열='<div>' + 한줄결과문자열 + '</div>';
-      정리결과합치기문자열=정리결과합치기문자열 + 한줄결과문자열;
-    }
-    정리결과합치기문자열='<div id="js_입항관리_정리">' + 정리결과합치기문자열 + '</div>'; //inline-block 설정을 위한 div 감싸기
-
-    document.querySelector('#풀기셑팅').innerHTML=왼쪽결과합치기문자열 + 정리결과합치기문자열;
-}
-
-
 
 //시작시 작동코드
 document.querySelector('#풀기셑팅').classList.add('d-none');
@@ -129,9 +84,6 @@ function 리스너_head_button_group더블클릭이벤트(e) {
   console.log('더블클릭:클릭 실행후 더블클릭 실행됨')
   if (e.target.innerHTML=='리스트') {
     풀기_리스트();
-  }
-  if (e.target.innerHTML=='입항관리') {
-    풀기_입항관리();
   }
 }
 
