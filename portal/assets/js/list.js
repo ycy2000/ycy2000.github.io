6b263a46cc0654e5d5d43af9bf14cab7bf95ae98
ycy2000 <ycy2000@naver.com>
2024-06-07 09:40:11 +0900

    SDFDF

--- portal/assets/js/list.js
+++ portal/assets/js/list.js
@@ -114,8 +114,9 @@ function 리스너_head_button_group클릭이벤트(e) {
   }
   if (e.target.innerHTML=='예정') {
     console.log('PNG_예정_셑팅')
-    document.querySelector('#풀기셑팅').classList.remove('d-none');
-    document.querySelector('#풀기셑팅').innerHTML='<img src="portal/images/문서연결_리스트/PNG_예정.png" alt="이미지없음">';
+    document.querySelector('#풀기셑팅').classList.add('d-none');
+    document.querySelector('#PNG셑팅').classList.remove('d-none');
+    document.querySelector('#PNG셑팅').innerHTML='<img src="portal/images/문서연결_리스트/PNG_예정.png" alt="이미지없음">';
   }
   if (e.target.innerHTML=='완료') {
     console.log('PNG_완료_셑팅')
