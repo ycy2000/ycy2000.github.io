468fd5496b93893e63a23c5e00ba4b0e47dcf486
ycy2000 <ycy2000@naver.com>
2025-02-26 10:49:16 +0900

    dfsfds

--- portal/assets/js/list.js
+++ portal/assets/js/list.js
@@ -288,27 +288,78 @@ function 리스트풀기() {
   자료풀림결과.innerHTML = '';
 
   //div안에 span에서  tr안에 td로 변경해보자 (복붙할때 엑셀에 테이블형식으로 되는지 확인인)
+  //첫정보 비어있을때 코드로 해결해보자 : 최대length파악, 현재length차이만큼 값을 넣는다??
+  var length최대=0;
   for (var i = 0; i < 리스트줄바꿈split.length - 1; i++) {
-    var div안span4 = '';
-    div안span4 = '<td>' + 리스트줄바꿈split[i].split('\t')[0] + '</td>'
-    div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[1] + '</td>'
-    div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[2] + '</td>'
-    div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[3] + '</td>'
-    div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[4] + '</td>'
-    div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[5] + '</td>'
-    div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[6] + '</td>'
-    div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[7] + '</td>'
-    div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[8] + '</td>' //입항일
-    div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[9] + '</td>' //구분(축산수산구분 제외외)
-    div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[10] + '</td>' //도착일
-    div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[11] + '</td>' //도착시간
-    div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[12] + '</td>' //구분(20,40피트), 13 화찰 제외
+    if (리스트줄바꿈split[i].split('\t').length>length최대) {
+      length최대=리스트줄바꿈split[i].split('\t').length;
+    }
+  }
 
-    div안span4 = '<table><tbody><tr>' + div안span4 + '</tr></tbody></table>'//이게 안들어가면 안되는데 왜인지 모르겠다.
-    자료풀림결과.innerHTML = 자료풀림결과.innerHTML + div안span4;
+  for (var i = 0; i < 리스트줄바꿈split.length - 1; i++) {
+    var length최대와차이=0;
+    var div안span4 = '';
+    length최대와차이=length최대-리스트줄바꿈split[i].split('\t').length;
+    if (length최대와차이==1) {
+      div안span4 = '<td></td>'
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[1] + '</td>'
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[2] + '</td>'
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[3] + '</td>'
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[4] + '</td>'
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[5] + '</td>'
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[6] + '</td>'
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[7] + '</td>'
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[8] + '</td>' //입항일
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[9] + '</td>' //구분(축산수산구분 제외외)
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[0] + '</td>' //도착일
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[10] + '</td>' //도착시간
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[11] + '</td>' //구분(20,40피트), 13 화찰 제외
+  
+      div안span4 = '<table><tbody><tr>' + div안span4 + '</tr></tbody></table>'//이게 안들어가면 안되는데 왜인지 모르겠다.
+      자료풀림결과.innerHTML = 자료풀림결과.innerHTML + div안span4;
+    }
+    if (length최대와차이==2) {
+      div안span4 = '<td></td>'
+      div안span4 += '<td></td>'
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[0] + '</td>'
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[1] + '</td>'
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[2] + '</td>'
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[3] + '</td>'
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[4] + '</td>'
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[5] + '</td>'
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[6] + '</td>' //입항일
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[7] + '</td>' //구분(축산수산구분 제외외)
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[8] + '</td>' //도착일
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[9] + '</td>' //도착시간
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[10] + '</td>' //구분(20,40피트), 13 화찰 제외
+  
+      div안span4 = '<table><tbody><tr>' + div안span4 + '</tr></tbody></table>'//이게 안들어가면 안되는데 왜인지 모르겠다.
+      자료풀림결과.innerHTML = 자료풀림결과.innerHTML + div안span4;
+    }
+    if (length최대와차이==0) {
+      div안span4 = '<td>' + 리스트줄바꿈split[i].split('\t')[0] + '</td>'
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[1] + '</td>'
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[2] + '</td>'
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[3] + '</td>'
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[4] + '</td>'
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[5] + '</td>'
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[6] + '</td>'
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[7] + '</td>'
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[8] + '</td>' //입항일
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[9] + '</td>' //구분(축산수산구분 제외외)
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[10] + '</td>' //도착일
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[11] + '</td>' //도착시간
+      div안span4 += '<td>' + 리스트줄바꿈split[i].split('\t')[12] + '</td>' //구분(20,40피트), 13 화찰 제외
+  
+      div안span4 = '<table><tbody><tr>' + div안span4 + '</tr></tbody></table>'//이게 안들어가면 안되는데 왜인지 모르겠다.
+      자료풀림결과.innerHTML = 자료풀림결과.innerHTML + div안span4;
+    }
   }
   자료풀림결과.innerHTML='<table><tbody>' + 자료풀림결과.innerHTML + '</tbody></table>'
 
+  //리스트단독 입항관리에 넣을때 사용할 요소다
+  document.querySelector('#리스트자료풀림결과js복사본').innerHTML=자료풀림결과.outerHTML;
+  
   자료풀림결과=document.querySelectorAll('#리스트자료풀림결과 tr');
 
   for (var i = 0; i < 자료풀림결과.length; i++) {
@@ -372,6 +423,96 @@ function 입항관리풀기() {
 리스트풀기()
 
 var 리스너png셑팅 = document.querySelector('#PNG셑팅');
+function 리스트단독입항관리맨아래에추가() {
+  console.log('리스트단독입항관리맨아래에추가()')
+  //입항관리자료는 전체문자열로 받아서 처리해야한다.
+  var 입항관리전체문자열 = document.querySelector('#입항관리도크부터도착항우측까지').innerHTML;
+  var 입항관리줄바꿈개수 = 입항관리전체문자열.split('\n');
+  var 입항관리bl_컨_결합문자열 = [];//컨_bl, 공백이 있을수 있지만 _ 는 있다. undefined 또는 공백
+  for (var i = 0; i < 입항관리줄바꿈개수.length; i++) {
+    입항관리bl_컨_결합문자열.push(입항관리줄바꿈개수[i].split('\t')[2] + '!' + 입항관리줄바꿈개수[i].split('\t')[10])
+  }
+  //배열을 다시 ! 로 join하여 하나의 문자열로 만든다. (컨과 bl이 느낌표로 결합된 하나의 문자열이 됨됨)
+  입항관리bl_컨_결합문자열 = 입항관리bl_컨_결합문자열.join('!');
+
+  var 리스트bl_컨_배열 = [];
+  var 단독담기배열=[];
+  //var 리스트목록한줄div = document.querySelectorAll('#PNG셑팅 #리스트자료풀림결과 tr');
+  var 리스트목록한줄div = document.querySelectorAll('#리스트자료풀림결과js복사본 tr');
+  var 리스트목록개수 = 리스트목록한줄div.length;
+  for (var i = 0; i < 리스트목록개수; i++) {
+    var 비엘 = 리스트목록한줄div[i].children[2].innerHTML.trim();
+    var 컨 = 리스트목록한줄div[i].children[4].innerHTML.trim();
+    if (비엘 == 'B/LNO.' || (비엘 == '' && 컨 == '')) { continue; } //다음반복문으로
+    //문자열.indexOf('찾는문자열') 없으면 -1
+    if (입항관리bl_컨_결합문자열.indexOf(비엘) > -1 || 입항관리bl_컨_결합문자열.indexOf(컨) > -1) {
+      //컨이나 bl중 하나라도 맞으면 건너뜀
+    } else {
+      console.log('단독')
+      var 단독배열push문자열=[]; //일단배열에 담아 가공한다.
+
+      //입항관리 뿌리는것 : 0도크,1화주,2컨,3작업순서,4상세,5,빈칸,6운송사,7,물품,8빈칸,9선사,10BL,11상세원본
+      단독배열push문자열.push(100);//0도크,순번부분
+      단독배열push문자열.push(리스트목록한줄div[i].children[3].innerHTML.trim());//1화주
+      단독배열push문자열.push(리스트목록한줄div[i].children[4].innerHTML.trim());//2컨
+      단독배열push문자열.push('작업미정');//3작업순서
+      단독배열push문자열.push(리스트목록한줄div[i].children[8].innerHTML.trim());//4상세. 리스트 입항일 값 넣는다
+      단독배열push문자열.push('');//5빈칸
+      단독배열push문자열.push('');//6운송사
+      단독배열push문자열.push(리스트목록한줄div[i].children[5].innerHTML.trim() + ' ' + 리스트목록한줄div[i].children[6].innerHTML.trim());//7물품, 품명 & 수량 넣는다
+      단독배열push문자열.push('');//8빈칸
+      단독배열push문자열.push(리스트목록한줄div[i].children[12].innerHTML.trim());//9선사(20피트표시에 선사메모사용)
+      단독배열push문자열.push(리스트목록한줄div[i].children[2].innerHTML.trim());//10BL
+      단독배열push문자열.push(리스트목록한줄div[i].children[7].innerHTML.trim());//11도착항
+      단독배열push문자열.push(리스트목록한줄div[i].children[8].innerHTML.trim());//12상세원본, 리스트 입항일 값 넣는다
+
+      단독배열push문자열=단독배열push문자열.join('\t');//탭으로 묶는다. 나중에 분리하여 사용할것이다
+      단독담기배열.push(단독배열push문자열);
+    }
+  }
+  if (단독담기배열.length==0) {return;}
+  //console.log(단독담기배열) : 입항관리에 단독만 표시되도록 코딩
+
+  var 자료풀림결과 = document.querySelector('#입항관리자료풀림결과');
+  자료풀림결과.innerHTML = '';
+  //1.화주,2.컨,4.상세,6.운송,7.물품,9.선명,10.BL,11.도착항,12.상세원본
+  for (var i = 0; i < 단독담기배열.length; i++) {
+    var div안span4 = '';
+    div안span4 = '<td>' + 단독담기배열[i].split('\t')[0] + '</td>'
+    div안span4 += '<td>' + 단독담기배열[i].split('\t')[1] + '</td>'
+    div안span4 += '<td>' + 단독담기배열[i].split('\t')[2] + '</td>'
+    div안span4 += '<td>' + 단독담기배열[i].split('\t')[3] + '</td>'
+    div안span4 += '<td>' + 단독담기배열[i].split('\t')[4] + '</td>'
+    div안span4 += '<td>' + 단독담기배열[i].split('\t')[5] + '</td>' //빈칸
+    div안span4 += '<td>' + 단독담기배열[i].split('\t')[6] + '</td>'
+    div안span4 += '<td>' + 단독담기배열[i].split('\t')[7] + '</td>'
+    div안span4 += '<td>' + 단독담기배열[i].split('\t')[8] + '</td>' //-2
+    div안span4 += '<td>' + 단독담기배열[i].split('\t')[9] + '</td>'
+    div안span4 += '<td>' + 단독담기배열[i].split('\t')[10] + '</td>'
+    div안span4 += '<td>' + 단독담기배열[i].split('\t')[11] + '</td>' //도착항
+    div안span4 += '<td>' + 단독담기배열[i].split('\t')[12] + '</td>' //상세내용원본
+    div안span4 += '<td></td>' //빈칸(상세원본복사쉽도록)
+    div안span4 = '<table><tbody><tr>' + div안span4 + '</tr></tbody></table>'//이게 안들어가면 안되는데 왜인지 모르겠다.
+    자료풀림결과.innerHTML = 자료풀림결과.innerHTML + div안span4;
+  }
+  자료풀림결과.innerHTML='<table><tbody>' + 자료풀림결과.innerHTML + '</tbody></table>'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+}
 function 입항관리에없는목록색칠() {
   var 개수 = document.querySelectorAll('.js한줄색칠있음').length;
   for (i = 0; i < 개수; i++) {
