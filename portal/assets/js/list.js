66d0c4b8aecdbf390cf8f790d63af2b3de5e2558
ycy2000 <ycy2000@naver.com>
2025-02-28 09:50:21 +0900

    DSFFSD

--- portal/assets/js/list.js
+++ portal/assets/js/list.js
@@ -166,7 +166,7 @@ function 첫번째버튼날짜있을때댤력에표시() {
 //메모 관련 끝..  
 
 //시작시 작동코드
-document.querySelector('#PNG셑팅').innerHTML = '<img src="portal/images/문서연결_리스트/_예정.png" alt="이미지없음">';
+document.querySelector('#PNG셑팅').innerHTML = '<img src="portal/images/문서연결_리스트/A예정.png" alt="이미지없음">';
 
 var 리스너_head_button_group = document.querySelector('#head_button_group');
 function 리스너_head_button_group클릭이벤트(e) {
