0303b62569f7775d139fb8b5e5f0f2be4d667097
ycy2000 <ycy2000@naver.com>
2025-04-01 16:36:00 +0900

    sdfsddsfsd

--- portal/assets/js/excel.js
+++ portal/assets/js/excel.js
@@ -1,39 +1,44 @@
 function 개인정리png(e) {
-  //<button title="들어갈id:img파일이름:가져올요소:js_자료형을먼저알아야겠다_설명:개인적정리png_this점key이름.png:없음" onclick="개인정리png(this)">'this.key이름'</button>
-  //<div id="js_자료형을먼저알아야겠다_설명" onclick="document.querySelector(`#${this.id}`).classList.add('d-none')" class="d-none"><img src="" alt="이미지없음">
-  //<!-- js로 아래에 들어갈 요소가 들어갈수 있음 --></div>
-  //onclick에 개인정리png(this) => this는 요소, function 개인정리png(e)에서 e가 요소가 됨
-  //onclick에 개인정리png(문자열) => typeof e = string, button이면 object
-  //<button title="들어갈id:img파일이름:가져올요소:java_개인적정리_1_자료형_png들어갈곳:개인적정리png_자료형.png:없음"
-  //   [0]들어갈id:[1]img파일이름:[2]가져올요소: =>자료설명
-  //   [3]java_개인적정리_1_자료형_png들어갈곳:[4]개인적정리png_자료형.png:[5]없음
-  //title을 split하면 [3]=들어갈곳id,[4]=png파일이름,[5]추가할 요소 (나중 위해)
+  //<div id="js_객체관련반복문_png들어갈곳" onclick="document.querySelector(`#${this.id}`).classList.add('d-none')" class="d-none"></div>
+  //<button title="들어갈곳id:없음(이름.png):없음(요소id)" onclick="개인정리png(this)"></button>
   var 타입typeof=typeof e; //요소이면 object 문자열이면 string
   var 타입proto=Object.prototype.toString.call(e).slice(8, -1); //그냥 확인용
   //console.log('typeof : ' + 타입typeof + ', proto : ' + 타입proto)
-  var 파일이름, 들어갈곳id;
+  var 파일이름, 들어갈곳id, 들어갈요소id;
 
   if (typeof e === 'string') { // '파일이름을 넣었을때'
     파일이름=e;
   }
   if (typeof e === 'object') { // 'this를 넣었을때'
-    파일이름=e.title.split(':')[4];
-    들어갈곳id=e.title.split(':')[3];
+    들어갈곳id=e.title.split(':')[0];
+    파일이름=e.title.split(':')[1];
+    들어갈요소id=e.title.split(':')[2];
   }
   var src값='portal/images/문서연결_엑셀VBA/' + 파일이름;
-  document.querySelector(`#${들어갈곳id} img`).src=src값;
-  document.querySelector(`#${들어갈곳id}`).classList.remove('d-none');
+
+  if (document.querySelector(`#${들어갈곳id}`)) {document.querySelector(`#${들어갈곳id}`).innerHTML=''}
+
+  if (파일이름!='없음') {
+    console.log('파일이름 : ' + 파일이름)
+    document.querySelector(`#${들어갈곳id}`).innerHTML='<img src="' + src값 + '" alt="이미지없음" style="vertical-align: top;margin-right:5px;"></img>';
+    document.querySelector(`#${들어갈곳id}`).classList.remove('d-none');
+  }
+  if (들어갈요소id!='없음') {
+    document.querySelector(`#${들어갈곳id}`).innerHTML+=document.querySelector(`#${들어갈요소id}`).outerHTML;
+    document.querySelector(`#${들어갈곳id}`).classList.remove('d-none');
+  }
 }
 
 function 연습2() {
-  const person = {이름 : "길동", 성 : "홍"};
-  let 프로토타입=Object.getPrototypeOf(person);
-  console.log( 프로토타입.keys);
-  console.log( 프로토타입.toString());
-  for (let 키 in 프로토타입) {console.log(키);}
-  for (let 키 of 프로토타입.keys()) {console.log(키);}
-  for (let 키 in 프로토타입.__proto__) {console.log(키);}
-  for (let 키 in 프로토타입) {console.log(키);}
+  function Parent() {
+    this.ownProperty = "I am own property";
+  }
+  Parent.prototype.inheritedProperty = "I am inherited property";
+  
+  const obj = new Parent();
+  const proto = Object.getPrototypeOf(obj);
+  console.log(Object.keys(proto)); // ['inheritedProperty']
+
 
 }
 
@@ -44,7 +49,7 @@ function 연습2() {
 function 연습() {
 
   var 결과부분 = document.querySelector('#전체대체');
-  var 연습제목설정이것만 = 'js_객체는속성과메서드를포함하는데이터의집합';
+  var 연습제목설정이것만 = 'js_객체관련반복문';
   var 연습시제목직접입력=document.querySelector('#선택문서제목');
   var 선택한캔버스관련자료none안_타겟element; 
   선택한캔버스관련자료none안_타겟element = document.querySelector('#' + 연습제목설정이것만);
