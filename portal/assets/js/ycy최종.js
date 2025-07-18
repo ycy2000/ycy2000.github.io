680929704f0c268497741b8a01b8d086c94db571
ycy2000 <ycy2000@naver.com>
2025-07-18 23:31:48 +0900

    ㄴㅇㄹㄴㅇㄹㄴㅇ

--- portal/assets/js/ycy최종.js
+++ portal/assets/js/ycy최종.js
@@ -1,28 +1,41 @@
 check_초기설정();
+회차select옵션생성();
 
+
+
+
+function 연습() {
+  var 문자열=document.querySelector('#당번숨김_안에_저장중').innerHTML;
+  문자열=문자열 + document.querySelector('#당번숨김_안에_기록중').innerHTML;
+  문자열=문자열.split('_');
+  var 결과문자열='';
+  for (var i=0; i<문자열.length; i++) {
+    if ((i+1)%9==0) {결과문자열+=문자열[i].trim()+',';} else {결과문자열+=문자열[i].trim()+'_';}
+    if ((i+1)%45==0) {결과문자열+='\n'}
+  }
+  document.querySelector('#메모').innerHTML=결과문자열;
+}
 function 색칠_1_동작설정() {
 }
 function 색칠_2_설정대로색칠동작() {
 }
-function 옵션생성() {
-}
+
 function html구조생성() {
 }
-function 회차change설정() {
+function 회차change설정(e) {
+  console.log(e.id)
   //당번회차change와 분석자료회차change 한개의 함수로 작성
 
 }
-function check_초기설정() {
-  //머리글(분류) + 분류 안에 소분류 : class="check클래스"가진 input의 title이 '분류', '소분류'의 id이다. d-none이 있는상태 (퍼덕거리는거 보기싫어서)
-  //체크this활용(e) 관련하여 : 인풋의 오른쪽형제인 lable에 'input의 id와 같은' for 생성, checked가 아닌경우 check_ 이후의 문자열(id) 요소에 d-none add
-  var check클래스=document.querySelectorAll('.check클래스');
-  for (var i=0; i<check클래스.length; i++) {check클래스[i].nextElementSibling.setAttribute('for',check클래스[i].id)}
-  for (var i=0; i<check클래스.length; i++) {if(check클래스[i].checked) {
-    document.querySelector('#' + check클래스[i].id.substring(6,check클래스[i].length)).classList.remove('d-none')}
-  }  
+function 회차select옵션생성() {
+  let arr = ($('#당번숨김_안에_저장중').html() + $('#당번숨김_안에_기록중').html()).split(','); arr.pop(); //마지막 배열값 제거(빈거)
+  let 옵션 = '';
+  for (let i = arr.length - 1; i >= 0; i--) 옵션 += `<option>${arr[i].split('_')[0]}</option>`;  
+  $('#당번_회차select, #분석자료_회차select').html(옵션);
 }
 function 체크this활용(e) {
-  //머리글(분류) + 분류 안에 소분류 : div에 걸었어도, 안의 input lable체크시 작동함. 보기/숨기기할 id는 input의 id에서 check_ 오른쪽 문자열
-  var 보기숨기기id='#' + e.children[0].id.substring(6,e.children[0].id.length); //  console.log('보기숨기기id : ' + 보기숨기기id)
-  if (e.children[0].checked) {document.querySelector(보기숨기기id).classList.remove('d-none');console.log('checked')} else {console.log('checked false');document.querySelector(보기숨기기id).classList.add('d-none')}
+  const $input = $(e).children('input');  const id = '#'+$input.attr('id').slice(6);  $(id).toggleClass('d-none', !$input.prop('checked'));
+}
+function check_초기설정() {
+  $('.check클래스').each( function() {$(this).next('label').attr('for', this.id); if (this.checked) $('#' + this.id.slice(6)).removeClass('d-none');}  )
 }
