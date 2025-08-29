7bb4459c230ccb8e7b94d239072a0b09686a74ce
ycy2000 <ycy2000@naver.com>
2025-08-29 16:44:16 +0900

    sdfsf

--- portal/assets/js/excel_코드점검.js
+++ portal/assets/js/excel_코드점검.js
@@ -22,73 +22,10 @@ function 카테고리배치(e) {
   console.log('카테고리배치(e)')
   document.querySelector('#캔버스바디').innerHTML=document.querySelector('#' + e.innerHTML).outerHTML
 }
-
-
-function 선택한캔버스_검색input_change시(e) {
-  console.log('캔버스_검색value_change시');
-  //추가 : 검색결과바탕색 클래스 TEXT만 남기기
-  var 검색결과바탕색_클래스들 = document.querySelectorAll('.검색결과바탕색');
-  for (var i=0; i<검색결과바탕색_클래스들.length; i++) {
-    검색결과바탕색_클래스들[i].outerHTML=검색결과바탕색_클래스들[i].innerHTML;
+var 리스너_바디=document.querySelector('body');
+function 리스너_바디_click(e) {
+  if (e.target.parentElement.parentElement.classList.contains('개별카테고리') && e.taget.title.trim().length>0) {
+    if (document.querySelector('#' + e.target.title)) {document.querySelector('#전체대체').innerHTML=document.querySelector('#' + e.target.title).outerHTML}
   }
-  //innerHTML로 검색한다. 메모도 검색해야하니까. 처음에만 두번표시한다?
-  var 검색할문자 = document.querySelector('#' + 선택한캔버스id + ' .canvas검색input').value.toUpperCase(); 
-  if (document.querySelector('#' + 선택한캔버스id + ' .canvas검색input').value == '') { return; }
-
-  var 찾는값=document.querySelector('#' + 선택한캔버스id + ' .canvas검색input').value; 
-  var 정규식내부= new RegExp('(?![^<]*>)' + 찾는값, 'ig')
-
-  //예전코드 대비 추가 1 : id(공백도 있으니 유의) 요소의 innerHTML에 검색문자 있을때 id 를 배열에 담기.
-  var 검색결과포함id배열=[];
-  // 해당 캔버스관련만 : var 검색할클래스들 = document.querySelectorAll('#' + 선택한캔버스id + '_관련자료none > [id]');
-
-  //id의 innerHTML에 찾는값 있을때 '아이디추출', 내부 값 색칠
-  var 검색할클래스들 = document.querySelectorAll('.모든검색 > [id]');
-  console.log(선택한캔버스id + ', id있는것개수 : ' + 검색할클래스들.length)  
-  for (var i = 0; i < 검색할클래스들.length; i++) {
-    if (검색할클래스들[i].id!='' && 검색할클래스들[i].innerHTML.toUpperCase().search(검색할문자) > -1) {
-      검색결과포함id배열.push(검색할클래스들[i].id);
-      if (검색할문자!=' ') {
-        검색할클래스들[i].innerHTML=
-        검색할클래스들[i].innerHTML.replace(정규식내부, '<span class="검색결과바탕색">' + 찾는값 + '</span>');
-      }
-    }
-  }
-  // title의 innerHTML에 찾는값 있을때 타이틀을 '검색결과포함id배열[]에 추가 내부 값 색칠
-  var 검색할클래스들 = document.querySelectorAll('.모든검색 .개별카테고리 [title]');
-  for (var i = 0; i < 검색할클래스들.length; i++) {
-    if (검색할클래스들[i].title!='' && 검색할클래스들[i].innerHTML.toUpperCase().search(검색할문자) > -1) {
-      검색결과포함id배열.push(검색할클래스들[i].title);
-      if (검색할문자!=' ') {
-        검색할클래스들[i].innerHTML=
-        검색할클래스들[i].innerHTML.replace(정규식내부, '<span class="검색결과바탕색">' + 찾는값 + '</span>');
-      }
-    }
-  }
-  //검색결과포함id배열[] 고유값 추출 var uniqueArray = [...new Set(myArray)]
-  검색결과포함id배열=[...new Set(검색결과포함id배열)];
-
-
-
-
-
-
-
-
-
-  // 해당 캔버스관련만 : var 검색할클래스들 = document.querySelectorAll('#' + 선택한캔버스id + '_관련자료none .개별카테고리 > div > h6');
-  var 검색할클래스들 = document.querySelectorAll('.모든검색 .개별카테고리 [title]');
-  var 내부html = '';
-  for (var i = 0; i < 검색할클래스들.length; i++) {
-  //예전코드 대비 추가 2 : if 조건 조정, 검색할클래스들의 title이 검색결과포함id배열 에 있으면 추가하는 코드는 먼저 진행하도록 한다  
-    if (검색결과포함id배열.includes(검색할클래스들[i].title)) {
-      내부html += 검색할클래스들[i].outerHTML;
-    }  
-  }
-  if (내부html == '') { alert('없음'); return; }
-  document.querySelector('#' + 선택한캔버스id + ' .캔버스바디').innerHTML = 내부html;
-  document.querySelector('#' + 선택한캔버스id + ' .canvas검색input').value = 검색할문자;
 }
-
-
-
+리스너_바디.addEventListener('click',리스너_바디_click)
