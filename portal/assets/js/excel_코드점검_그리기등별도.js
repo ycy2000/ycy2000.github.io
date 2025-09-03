d6dc5fbff921ac1b0261bfabc89a147cf60e0142
ycy2000 <ycy2000@naver.com>
2025-09-03 21:06:48 +0900

    fgdgfd

--- portal/assets/js/excel_코드점검_그리기등별도.js
+++ portal/assets/js/excel_코드점검_그리기등별도.js
@@ -1,6 +1,8 @@
 function 캔버스_개별카테고리_h6의title과id순서() {
   //id나열
   var 아이디=Array.from(document.querySelectorAll('.모든id모음위치자유 > [id]'), (요소,index) => 요소.id); 
+  var h6정보=Array.from(document.querySelectorAll('.개별카테고리 > div > [title]'));
+  var 타이틀=Array.from(document.querySelectorAll('.개별카테고리 > div > [title]'), (요소,index) => 요소.title); 
   var 넣을곳=document.querySelector('#id나열');넣을곳.innerHTML='';
   document.querySelector('#id개수').innerHTML=아이디.length;
   아이디.forEach ( (value,index,array) => {
@@ -19,6 +21,7 @@ function 캔버스_개별카테고리_h6의title과id순서() {
     div.appendChild(span);
     var span=document.createElement('span');
     span.innerText=value; //value는 id
+    if (!타이틀.includes(value)) {span.setAttribute('class','노랑바탕색')}
     div.appendChild(span);
     넣을곳.appendChild(div);
   });
@@ -32,12 +35,55 @@ function 캔버스_개별카테고리_h6의title과id순서() {
     요소.children[0].setAttribute('style','width:' + Math.max(...첫번째width) + 'px');
     요소.children[1].setAttribute('style','width:' + Math.max(...두번째width) + 'px');
   });
-  //h6의 title 부분
+  //h6의 title 부분 : {캔버스id,카테고리id,title} 따오기
+  var 캔버스id, 카테고리id, title;
   var h6정보3종=[];
-  var h6정보=Array.from(document.querySelectorAll('.개별카테고리 > div > [title]'));
+  // 위에 정의 : var h6정보=Array.from(document.querySelectorAll('.개별카테고리 > div > [title]'));
+  document.querySelector('#title개수').innerHTML=h6정보.length;
   h6정보.forEach ( (요소,인덱스,array) => {
-    h6정보3종.push({요소,인덱스,array})
-
+    캔버스id=요소.parentElement.parentElement.parentElement.id;
+    카테고리id=요소.parentElement.parentElement.id;
+    title=요소.title;
+    //h6정보3종.push({'캔버스id':캔버스id,'카테고리id':카테고리id,'title':title})
+    h6정보3종.push({캔버스id,카테고리id,title})
   })
-  console.log(h6정보3종)
+  var 카운트=0;
+  var 넣을곳=document.querySelector('#title나열');넣을곳.innerHTML='';
+  for (const { 캔버스id,카테고리id,title } of h6정보3종) {
+    카운트+=1;
+    if (카운트==1) {
+      var div=document.createElement('div');
+      Array.from({length:3}).forEach ( (ele,내부index) => {
+        var span=document.createElement('span');
+        span.innerText=['캔버스id','카테고리id','title'][내부index];
+        div.appendChild(span);
+      })
+      넣을곳.appendChild(div);
+    }
+    var div=document.createElement('div');
+    var span=document.createElement('span');
+    span.innerText=캔버스id;
+    div.appendChild(span);
+    var span=document.createElement('span');
+    span.innerText=카테고리id; 
+    div.appendChild(span);
+    var span=document.createElement('span');
+    span.innerText=title; 
+    if (!아이디.includes(title)) {span.setAttribute('class','노랑바탕색')}
+    div.appendChild(span);
+    넣을곳.appendChild(div);
+  };
+  var 첫번째width=[];
+  var 두번째width=[];
+  var 세번째width=[];
+  Array.from(document.querySelectorAll('#title나열 > div')).forEach ( (요소,index,array) => {
+    첫번째width.push(요소.children[0].clientWidth);
+    두번째width.push(요소.children[1].clientWidth);
+    세번째width.push(요소.children[2].clientWidth);
+  });
+  Array.from(document.querySelectorAll('#title나열 > div')).forEach ( (요소,index,array) => {
+    요소.children[0].setAttribute('style','width:' + Math.max(...첫번째width) + 'px');
+    요소.children[1].setAttribute('style','width:' + Math.max(...두번째width) + 'px');
+    요소.children[2].setAttribute('style','width:' + Math.max(...세번째width) + 'px');
+  });
 }
