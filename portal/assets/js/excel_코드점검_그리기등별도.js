e7f1ad8eb3644521170b0de6d1310ef8df42174c
ycy2000 <ycy2000@naver.com>
2025-09-03 16:41:26 +0900

    ㅇㄴㄹㄴㅇㄹ

--- portal/assets/js/excel_코드점검_그리기등별도.js
+++ portal/assets/js/excel_코드점검_그리기등별도.js
@@ -1,7 +1,6 @@
 function 캔버스_개별카테고리_h6의title과id순서() {
-  //한줄 : div 안에 span 7개
-  var 아이디=Array.from(document.querySelectorAll('.모든id모음위치자유 > [id]'), (요소,index) => 요소.id);
-  var 캔버스=Array.from(document.querySelectorAll('.캔버스'), (요소,index) => 요소.id);
+  //id나열
+  var 아이디=Array.from(document.querySelectorAll('.모든id모음위치자유 > [id]'), (요소,index) => 요소.id); 
   var 넣을곳=document.querySelector('#id나열');넣을곳.innerHTML='';
   document.querySelector('#id개수').innerHTML=아이디.length;
   아이디.forEach ( (value,index,array) => {
@@ -26,13 +25,14 @@ function 캔버스_개별카테고리_h6의title과id순서() {
   var 첫번째width=[];
   var 두번째width=[];
   Array.from(document.querySelectorAll('#id나열 > div')).forEach ( (요소,index,array) => {
-    첫번째width.push(요소.children[1].clientWidth);
+    첫번째width.push(요소.children[0].clientWidth);
     두번째width.push(요소.children[1].clientWidth);
   });
   Array.from(document.querySelectorAll('#id나열 > div')).forEach ( (요소,index,array) => {
-    요소.children[0].style.width=Math.max(첫번째width) + 'px';
-    요소.children[1].style.width=Math.max(두번째width) + 'px';
+    요소.children[0].setAttribute('style','width:' + Math.max(...첫번째width) + 'px');
+    요소.children[1].setAttribute('style','width:' + Math.max(...두번째width) + 'px');
   });
 
-
+  //h6의 title 부분
+  var 캔버스=Array.from(document.querySelectorAll('.캔버스'), (요소,index) => 요소.id);
 }
