cc20dbd17820a6bd9c805ad8b7a8f56fd6842f5f
ycy2000 <ycy2000@naver.com>
2026-01-18 14:26:31 +0900

    sdfsdf

--- unipass_server/server.js
+++ unipass_server/server.js
@@ -1,21 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
-
 const app = express();
 
-// index.html 제공, http://localhost:3000 주소창으로 접근시 node_custom.html 켜짐
-//app.get('/', (req, res) => { 
-//    res.sendFile(path.join(__dirname, 'node_custom.html'));
-//});
-
-// 출처제한 해제, 다른폴더에서 브라우저 파일 제공시 다른출처이다
-app.use((req, res, next) => {
-   res.setHeader("Access-Control-Allow-Origin", "*");
-   next();
-});
-
+app.use(cors());
+console.log('d');
+//브라우저 : const res = await fetch(`/unipass?blno=${blno}&year=${year}`);
 // UNIPASS 중계
 app.get('/unipass', async (req, res) => {
+        res.send('text');
     const 관세청출처 = 'https://unipass.customs.go.kr:38010';
     const 관세청api서비스명_통관진행정보 = 
     '/ext/rest/cargCsclPrgsInfoQry/retrieveCargCsclPrgsInfo?';
@@ -23,8 +16,8 @@ app.get('/unipass', async (req, res) => {
     const BLNO = req.query.blno;
     const year = req.query.year;
 
-    const url = `${관세청출처}${관세청api서비스명_통관진행정보}
-    crkyCn=${관세청승인키}&mblNo=${BLNO}&blYy=${year}`;
+
+    const url = `${관세청출처}${관세청api서비스명_통관진행정보}crkyCn=${관세청승인키}&mblNo=${BLNO}&blYy=${year}`;
 
     const response = await fetch(url);  // Node 내장 fetch
     const text = await response.text();
