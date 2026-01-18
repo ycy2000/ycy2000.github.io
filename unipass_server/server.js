602ccb1b8cd5f452bed49bfaa67d4f88f1abcc20
ycy2000 <ycy2000@naver.com>
2026-01-18 13:24:23 +0900

    vdsfddf

--- unipass_server/server.js
+++ unipass_server/server.js
@@ -16,10 +16,15 @@ app.use((req, res, next) => {
 
 // UNIPASS 중계
 app.get('/unipass', async (req, res) => {
+    const 관세청출처 = 'https://unipass.customs.go.kr:38010';
+    const 관세청api서비스명_통관진행정보 = 
+    '/ext/rest/cargCsclPrgsInfoQry/retrieveCargCsclPrgsInfo?';
+    const 관세청승인키 = 'c290s255d192h253h000l090g5';
     const BLNO = req.query.blno;
     const year = req.query.year;
 
-    const url = `https://unipass.customs.go.kr:38010/ext/rest/cargCsclPrgsInfoQry/retrieveCargCsclPrgsInfo?crkyCn=c290s255d192h253h000l090g5&mblNo=${BLNO}&blYy=${year}`;
+    const url = `${관세청출처}${관세청api서비스명_통관진행정보}
+    crkyCn=${관세청승인키}&mblNo=${BLNO}&blYy=${year}`;
 
     const response = await fetch(url);  // Node 내장 fetch
     const text = await response.text();
