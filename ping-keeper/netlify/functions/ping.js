a81809545d3b72e0145f3abfe98366539753c27e
ycy2000 <ycy2000@naver.com>
2026-01-26 16:22:09 +0900

    sdfsf

--- ping-keeper/netlify/functions/ping.js
+++ ping-keeper/netlify/functions/ping.js
@@ -1,7 +1,7 @@
 const https = require('https');
 
 exports.handler = async () => {
-  const url = 'https://myapp.onrender.com'; // ← 본인 Render 주소
+  const url = 'https://dongil-server.onrender.com'; // ← 본인 Render 주소
 
   return new Promise((resolve, reject) => {
     const req = https.get(url, (res) => {
