707e18bedbf80ca130d390dc58acdc0a1c681859
ycy2000 <ycy2000@naver.com>
2026-06-12 17:04:33 +0900

    0612금

--- portal/assets/js/excel_코드점검.js
+++ portal/assets/js/excel_코드점검.js
@@ -1,6 +1,6 @@
 let 전체변수h6title='';
 function 특정id편집() {
-  전체변수h6title ='주식_LP유동성공급자의의무' //원래는 h6의 title이다.
+  전체변수h6title ='객체_CreateObjectAndNew' //원래는 h6의 title이다.
   전체대체에셑팅();
 }
 function 이동배치(요소의타이틀,배치요소아이디) {
